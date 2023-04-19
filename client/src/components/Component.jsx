5f7b8150801f09cb26d7d4d0c33f04b986096b52
Ekanshu Joshi <99767187+ekanshuj@users.noreply.github.com>
2023-04-19 17:05:34 +0530

    ui-fixes

--- client/src/components/Component.jsx
+++ client/src/components/Component.jsx
@@ -32,23 +32,24 @@ const Component = () => {
 
   return (
     <>
-      <section className="bg-gray-500 body-font py-8 my-1">
-        <form onSubmit={handleSubmit(onSubmit)} className="flex items-center justify-center flex-col max-w-7xl mx-auto">
-          <div className='my-4'>
-            <p className='text-[1.25rem] sm:text-[1.45rem] tracking-[0.24rem] font-extrabold uppercase'>Enter Image Details</p>
+      <section className="bg-gray-500 body-font py-8 my-1 px-1 sm:px-0">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex items-center justify-center flex-wrap max-w-xl mx-auto">
+          <div className='flex-[1_1_20rem]'>
+            <div className='my-4 text-center'>
+              <p className='text-xl sm:text-2xl font-bold tracking-wide underline uppercase'>Enter Image Details</p>
+            </div>
+            <div className="my-1">
+              <input type="text" id="prompt" name="prompt" placeholder='Describe your image' {...register("prompt")} className="w-full bg-[ghostwhite] rounded border border-gray-300 text-base outline-none text-gray-700 py-2 px-3 leading-8" />
+            </div>
+            <div className="my-1">
+              <select required name="size" id="size" className="w-full bg-[ghostwhite] rounded border border-gray-300 text-base outline-none text-gray-700 py-3 px-3 leading-8" {...register("size")}>
+                <option value="256x256">256x256</option>
+                <option value="512x512">512x512</option>
+                <option value="1024x1024">1024x1024</option>
+              </select>
+            </div>
+            <button type='submit' className="text-white bg-black border-0 py-3 px-4 rounded text-lg my-2 font-medium w-full">Generate Image</button>
           </div>
-          <div className="w-[20rem] sm:w-[27rem] my-1">
-            <input type="text" id="prompt" name="prompt" placeholder='Describe your image' {...register("prompt")} className="w-full bg-[ghostwhite] rounded border border-gray-300 text-base outline-none text-gray-700 py-1 px-3 leading-8" />
-          </div>
-          <div className="w-[20rem] sm:w-[27rem] my-1">
-            <select name="size" id="size" className="w-full bg-[ghostwhite] rounded border border-gray-300 text-base outline-none text-gray-700 py-1 px-3 leading-8" placeholder='Your preferred size' {...register("size")}>
-              <option>Your preferred size</option>
-              <option value="256x256">256x256</option>
-              <option value="512x512">512x512</option>
-              <option value="1024x1024">1024x1024</option>
-            </select>
-          </div>
-          <button type='submit' className="text-white bg-black border-0 py-1 px-4 rounded text-lg my-2 font-medium">Generate Image</button>
         </form>
       </section>
       {isSubmitting ? <div className='flex items-center justify-center my-3'>
@@ -57,7 +58,7 @@ const Component = () => {
           size={10}
         />
       </div> :
-        URL && <Image url={URL} alt={text} />}
+        URL !== null && <Image url={URL} alt={text} />}
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className='flex justify-between items-center p-1 bg-black text-white'>
-      <p className='text-lg font-bold'>OpenAI Image Generator</p>
+    <header className='flex flex-col sm:flex-row justify-between items-center px-1 py-3 bg-black text-white'>
+      <p className='text-xl sm:text-2xl font-semibold tracking-wider'>OpenAI Image Generator</p>
       <a href="https://beta.openai.com/docs/introduction/overview">
-        <p className='text-[1.15rem] font-medium cursor-pointer'>Docs</p>
+        <p className='text-xl sm:text-2xl font-medium cursor-pointer underline tracking-wider'>Docs</p>
       </a>
     </header>
   )
